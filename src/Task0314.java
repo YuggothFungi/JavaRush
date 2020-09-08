f89f5a4c3cf94593500e603e033d158a6c9572d7
SirMax <maxxx.soloviev@yandex.ru>
2020-09-08 15:53:56 +0300

    Task0314

---
+++ src/Task0314.java
@@ -0,0 +1,33 @@
+public class Task0314 {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 10; a++)
+            System.out.print(a + " ");
+        System.out.println();
+        for (int a = 2; a <= 20; a = a + 2)
+            System.out.print(a + " ");
+        System.out.println();
+        for (int a = 3; a <= 30; a = a + 3)
+            System.out.print(a + " ");
+        System.out.println();
+        for (int a = 4; a <= 40; a = a + 4)
+            System.out.print(a + " ");
+        System.out.println();
+        for (int a = 5; a <= 50; a = a + 5)
+            System.out.print(a + " ");
+        System.out.println();
+        for (int a = 6; a <= 60; a = a + 6)
+            System.out.print(a + " ");
+        System.out.println();
+        for (int a = 7; a <= 70; a = a + 7)
+            System.out.print(a + " ");
+        System.out.println();
+        for (int a = 8; a <= 80; a = a + 8)
+            System.out.print(a + " ");
+        System.out.println();
+        for (int a = 9; a <= 90; a = a + 9)
+            System.out.print(a + " ");
+        System.out.println();
+        for (int a = 10; a <= 100; a = a + 10)
+            System.out.print(a + " ");
+    }
+}
