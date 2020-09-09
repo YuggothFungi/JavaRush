f7e161963292162179c3338bead4480a7dcb7670
SirMax <maxxx.soloviev@yandex.ru>
2020-09-09 21:08:13 +0300

    Task0431

---
+++ src/Task0431.java
@@ -0,0 +1,14 @@
+
+/* 
+От 10 до 1
+*/
+
+public class Task0431 {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
