e0215c1dcafadb0ad70ea2aed5e1d5c7b6e2f94a
SirMax <maxxx.soloviev@yandex.ru>
2020-09-09 22:24:36 +0300

    Task0433

---
+++ src/Task0433.java
@@ -0,0 +1,15 @@
+/* 
+Гадание на долларовый счет
+*/
+
+public class Task0433 {
+    public static void main(String[] args) {
+        String a = "SSSSSSSSSS";
+        int b = 1;
+
+        while (b <= 10) {
+            System.out.println(a);
+            b++;
+        }
+    }
+}
